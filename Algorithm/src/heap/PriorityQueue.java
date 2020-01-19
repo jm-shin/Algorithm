2ee76dd57c45e339387179ce831adb9480559123
jongminshin <ace4463@gmail.com>
2020-01-19 18:57:58 +0900

    heap

---
+++ Algorithm/src/heap/PriorityQueue.java
@@ -0,0 +1,30 @@
+package heap;
+
+public class PriorityQueue {
+
+	class Solution {
+	    public int[] solution(String[] operations) {
+	        int[] answer = {};
+	        return answer;
+	        
+	        PriorityQueue<operations> pq = new PriorityQueue();
+	        
+	       while() {
+	    	   if(pq.equals("I")) {
+	    		   
+	    	   }
+	    	   if() {
+	    		   
+	    	   }
+	    	   if() {
+	    		   
+	    	   }
+	       }
+	        
+	        
+	    }
+	}
+	public static void main(String[] args) {
+		Solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]);
+	}
+}
