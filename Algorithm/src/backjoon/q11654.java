70b4936a23d3f16c79a57f3eab41de5b46f9c0a1
jongminshin <ace4463@gmail.com>
2020-04-01 17:05:50 +0900

    backjoon 2798,2558,2798

---
+++ Algorithm/src/backjoon/q11654.java
@@ -0,0 +1,12 @@
+public class q11654 {
+ 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String ascii = sc.next();
+        int result = ascii.charAt(0);
+        
+        System.out.println(result);
+        
+    }
+}
