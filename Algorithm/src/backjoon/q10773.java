9d43b6e6988947953b40137a49804a86f5b40cec
jongminshin <ace4463@gmail.com>
2020-04-26 23:35:28 +0900

    10773번

---
+++ Algorithm/src/backjoon/q10773.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Stack;
+
+public class q10773 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i, t, sum = 0;
+        /* input */ int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
+        /* stack */
+        for (i = 0; i < N; i++) {
+            t = Integer.parseInt(br.readLine());
+            if (t == 0 && !stack.isEmpty()) {
+                stack.pop();
+            } else if (t != 0) {
+                stack.push(t);
+            }
+        }
+
+        /* sum */ while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
+}
