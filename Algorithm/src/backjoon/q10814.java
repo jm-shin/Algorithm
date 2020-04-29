e0832d34c1c17883bf9e17ef3cd56474228eb870
jongminshin <ace4463@gmail.com>
2020-04-29 21:42:12 +0900

    10814문제

---
+++ Algorithm/src/backjoon/q10814.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class q10814 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		
+		int n= sc.nextInt();
+		
+		String arr[][]=new String[n][2]; // 나이, 이름
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<2;j++) {
+				arr[i][j] = sc.next();			
+			}
+			
+		}
+	
+		
+		Arrays.sort(arr,new Comparator<String[]>() { // 0:나이 , 1:이름
+
+			@Override
+			public int compare(String[] o1,String[] o2) {
+											
+					return Integer.compare(Integer.parseInt(o1[0]), Integer.parseInt(o2[0]));
+			
+			}		
+			
+		});
+		
+		for(int i=0;i<arr.length;i++) {
+	
+				System.out.println(arr[i][0]+" "+arr[i][1]);
+		
+		}
+		
+	}
+	
+}
