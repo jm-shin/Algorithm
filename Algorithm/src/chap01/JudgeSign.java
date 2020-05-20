a61680108e230bc65584915f9b047d6e0dac32c6
jongminshin <ace4463@gmail.com>
2020-05-20 23:03:48 +0900

    JudgeSign

---
+++ Algorithm/src/chap01/JudgeSign.java
@@ -0,0 +1,16 @@
+package chap01;
+import java.util.*;
+
+public class JudgeSign {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하세요.");
+	    int n = scan.nextInt();
+	    
+	    if(n > 0)
+	    	System.out.println("이 정수는 양수입니다.");
+	    else if(n < 0)
+	    	System.out.println("이 정수는 음수입니다.");
+	}
+}
